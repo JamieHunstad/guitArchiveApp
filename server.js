559431a1a4259b7d516b099e88d49cbecb632ebf
JamieHunstad <jamie.hunstad@gmail.com>
2023-12-14 01:21:00 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
+const express = require('express');
 const app = express();
 
-// Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/guitarchive_frontend'));
+app.use(express.static('./dist/'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/guitarchive_frontend/index.html'));
+app.get('/*', (req, res) => {
+  res.sendFile('index.html', { root: 'dist/' });
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Server is running on port ${process.env.PORT || 8080}`);
-});
+app.listen(process.env.PORT || 8080);
