01e90f79796343f46da5269fa0caa59de6dfb347
JamieHunstad <jamie.hunstad@gmail.com>
2023-12-14 01:25:37 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('./dist/'));
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, 'dist')));
 
+// Catch all other routes and serve 'index.html'
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+  res.sendFile('index.html', { root: path.join(__dirname, 'dist') });
 });
 
-app.listen(process.env.PORT || 8080);
+// Start the server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
