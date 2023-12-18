7348572e7c21cac575ed75ce69afced69f4882a8
JamieHunstad <jamie.hunstad@gmail.com>
2023-12-17 21:16:29 -0600

    updating api path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: "https://guitarchive-service-4085e6c7378c.herokuapp.com/"
+  apiUrl: "https://guitarchive-service-4085e6c7378c.herokuapp.com"
 };
 
 /*
